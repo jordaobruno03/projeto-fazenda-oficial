db3e9a284c23297f11a05b6db4171ad0f6a50746
jordaobruno03 <164689481+jordaobruno03@users.noreply.github.com>
2024-10-08 18:43:22 -0300

    Criação do Menu Principal
    Criação do Menu Principal

--- .vs/Sistema  Fazenda/Sistema  Fazenda/MenuPrincipal.Designer.cs
+++ .vs/Sistema  Fazenda/Sistema  Fazenda/MenuPrincipal.Designer.cs
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnProdutosMenu = new System.Windows.Forms.Button();
+            this.btnClientesMenu = new System.Windows.Forms.Button();
+            this.btnFornecedoresMenu = new System.Windows.Forms.Button();
+            this.btnRelatorios = new System.Windows.Forms.Button();
+            this.btnPedidosMenu = new System.Windows.Forms.Button();
+            this.lblMenuPrincipalTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnProdutosMenu
+            // 
+            this.btnProdutosMenu.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnProdutosMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProdutosMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProdutosMenu.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnProdutosMenu.Location = new System.Drawing.Point(55, 73);
+            this.btnProdutosMenu.Name = "btnProdutosMenu";
+            this.btnProdutosMenu.Size = new System.Drawing.Size(270, 60);
+            this.btnProdutosMenu.TabIndex = 0;
+            this.btnProdutosMenu.Text = "Produtos";
+            this.btnProdutosMenu.UseVisualStyleBackColor = false;
+            // 
+            // btnClientesMenu
+            // 
+            this.btnClientesMenu.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnClientesMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClientesMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClientesMenu.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnClientesMenu.Location = new System.Drawing.Point(55, 373);
+            this.btnClientesMenu.Name = "btnClientesMenu";
+            this.btnClientesMenu.Size = new System.Drawing.Size(270, 60);
+            this.btnClientesMenu.TabIndex = 1;
+            this.btnClientesMenu.Text = "Clientes";
+            this.btnClientesMenu.UseVisualStyleBackColor = false;
+            // 
+            // btnFornecedoresMenu
+            // 
+            this.btnFornecedoresMenu.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnFornecedoresMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFornecedoresMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFornecedoresMenu.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnFornecedoresMenu.Location = new System.Drawing.Point(55, 173);
+            this.btnFornecedoresMenu.Name = "btnFornecedoresMenu";
+            this.btnFornecedoresMenu.Size = new System.Drawing.Size(270, 60);
+            this.btnFornecedoresMenu.TabIndex = 2;
+            this.btnFornecedoresMenu.Text = "Fornecedores";
+            this.btnFornecedoresMenu.UseVisualStyleBackColor = false;
+            // 
+            // btnRelatorios
+            // 
+            this.btnRelatorios.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnRelatorios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRelatorios.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRelatorios.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRelatorios.Location = new System.Drawing.Point(55, 473);
+            this.btnRelatorios.Name = "btnRelatorios";
+            this.btnRelatorios.Size = new System.Drawing.Size(270, 60);
+            this.btnRelatorios.TabIndex = 3;
+            this.btnRelatorios.Text = "Relatórios";
+            this.btnRelatorios.UseVisualStyleBackColor = false;
+            // 
+            // btnPedidosMenu
+            // 
+            this.btnPedidosMenu.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnPedidosMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPedidosMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPedidosMenu.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnPedidosMenu.Location = new System.Drawing.Point(55, 273);
+            this.btnPedidosMenu.Name = "btnPedidosMenu";
+            this.btnPedidosMenu.Size = new System.Drawing.Size(270, 60);
+            this.btnPedidosMenu.TabIndex = 4;
+            this.btnPedidosMenu.Text = "Pedidos";
+            this.btnPedidosMenu.UseVisualStyleBackColor = false;
+            // 
+            // lblMenuPrincipalTitulo
+            // 
+            this.lblMenuPrincipalTitulo.AutoSize = true;
+            this.lblMenuPrincipalTitulo.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMenuPrincipalTitulo.Location = new System.Drawing.Point(103, 20);
+            this.lblMenuPrincipalTitulo.Name = "lblMenuPrincipalTitulo";
+            this.lblMenuPrincipalTitulo.Size = new System.Drawing.Size(168, 36);
+            this.lblMenuPrincipalTitulo.TabIndex = 5;
+            this.lblMenuPrincipalTitulo.Text = "Menu Principal";
+            // 
+            // MenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(384, 561);
+            this.Controls.Add(this.lblMenuPrincipalTitulo);
+            this.Controls.Add(this.btnPedidosMenu);
+            this.Controls.Add(this.btnRelatorios);
+            this.Controls.Add(this.btnFornecedoresMenu);
+            this.Controls.Add(this.btnClientesMenu);
+            this.Controls.Add(this.btnProdutosMenu);
+            this.Name = "MenuPrincipal";
             this.Text = "MenuPrincipal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnProdutosMenu;
+        private System.Windows.Forms.Button btnClientesMenu;
+        private System.Windows.Forms.Button btnFornecedoresMenu;
+        private System.Windows.Forms.Button btnRelatorios;
+        private System.Windows.Forms.Button btnPedidosMenu;
+        private System.Windows.Forms.Label lblMenuPrincipalTitulo;
     }
 }
