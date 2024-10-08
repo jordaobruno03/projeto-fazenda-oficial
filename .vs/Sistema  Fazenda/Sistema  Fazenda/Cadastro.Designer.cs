9c8cdc7a63ca9c5153bde217251cb0ba752c1713
jordaobruno03 <164689481+jordaobruno03@users.noreply.github.com>
2024-10-08 19:00:38 -0300

    Criei a tela Cadastre-se
    Criei a tela Cadastre-se

---
+++ .vs/Sistema  Fazenda/Sistema  Fazenda/Cadastro.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Sistema__Fazenda
+{
+    partial class Cadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUsuarioCadastro = new System.Windows.Forms.Label();
+            this.lblSenhaCadastro = new System.Windows.Forms.Label();
+            this.lblConfirmarSenhaCadastro = new System.Windows.Forms.Label();
+            this.txtUsuarioCadastro = new System.Windows.Forms.TextBox();
+            this.txtSenhaCadastro = new System.Windows.Forms.TextBox();
+            this.txtConfirmarSenhaCadastro = new System.Windows.Forms.TextBox();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnIncluirCadastro = new System.Windows.Forms.Button();
+            this.lblCadastreseTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblUsuarioCadastro
+            // 
+            this.lblUsuarioCadastro.AutoSize = true;
+            this.lblUsuarioCadastro.Location = new System.Drawing.Point(78, 76);
+            this.lblUsuarioCadastro.Name = "lblUsuarioCadastro";
+            this.lblUsuarioCadastro.Size = new System.Drawing.Size(46, 13);
+            this.lblUsuarioCadastro.TabIndex = 0;
+            this.lblUsuarioCadastro.Text = "Usuário:";
+            // 
+            // lblSenhaCadastro
+            // 
+            this.lblSenhaCadastro.AutoSize = true;
+            this.lblSenhaCadastro.Location = new System.Drawing.Point(83, 146);
+            this.lblSenhaCadastro.Name = "lblSenhaCadastro";
+            this.lblSenhaCadastro.Size = new System.Drawing.Size(41, 13);
+            this.lblSenhaCadastro.TabIndex = 1;
+            this.lblSenhaCadastro.Text = "Senha:";
+            // 
+            // lblConfirmarSenhaCadastro
+            // 
+            this.lblConfirmarSenhaCadastro.AutoSize = true;
+            this.lblConfirmarSenhaCadastro.Location = new System.Drawing.Point(44, 216);
+            this.lblConfirmarSenhaCadastro.Name = "lblConfirmarSenhaCadastro";
+            this.lblConfirmarSenhaCadastro.Size = new System.Drawing.Size(86, 13);
+            this.lblConfirmarSenhaCadastro.TabIndex = 2;
+            this.lblConfirmarSenhaCadastro.Text = "Confirmar senha:";
+            // 
+            // txtUsuarioCadastro
+            // 
+            this.txtUsuarioCadastro.Location = new System.Drawing.Point(130, 69);
+            this.txtUsuarioCadastro.Name = "txtUsuarioCadastro";
+            this.txtUsuarioCadastro.Size = new System.Drawing.Size(300, 20);
+            this.txtUsuarioCadastro.TabIndex = 3;
+            // 
+            // txtSenhaCadastro
+            // 
+            this.txtSenhaCadastro.Location = new System.Drawing.Point(130, 139);
+            this.txtSenhaCadastro.Name = "txtSenhaCadastro";
+            this.txtSenhaCadastro.Size = new System.Drawing.Size(300, 20);
+            this.txtSenhaCadastro.TabIndex = 4;
+            // 
+            // txtConfirmarSenhaCadastro
+            // 
+            this.txtConfirmarSenhaCadastro.Location = new System.Drawing.Point(130, 209);
+            this.txtConfirmarSenhaCadastro.Name = "txtConfirmarSenhaCadastro";
+            this.txtConfirmarSenhaCadastro.Size = new System.Drawing.Size(300, 20);
+            this.txtConfirmarSenhaCadastro.TabIndex = 5;
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Location = new System.Drawing.Point(45, 267);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(75, 23);
+            this.btnVoltar.TabIndex = 6;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            // 
+            // btnIncluirCadastro
+            // 
+            this.btnIncluirCadastro.Location = new System.Drawing.Point(352, 267);
+            this.btnIncluirCadastro.Name = "btnIncluirCadastro";
+            this.btnIncluirCadastro.Size = new System.Drawing.Size(107, 23);
+            this.btnIncluirCadastro.TabIndex = 7;
+            this.btnIncluirCadastro.Text = "Incluir Cadastro";
+            this.btnIncluirCadastro.UseVisualStyleBackColor = true;
+            // 
+            // lblCadastreseTitulo
+            // 
+            this.lblCadastreseTitulo.AutoSize = true;
+            this.lblCadastreseTitulo.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 21F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCadastreseTitulo.Location = new System.Drawing.Point(171, 9);
+            this.lblCadastreseTitulo.Name = "lblCadastreseTitulo";
+            this.lblCadastreseTitulo.Size = new System.Drawing.Size(139, 36);
+            this.lblCadastreseTitulo.TabIndex = 8;
+            this.lblCadastreseTitulo.Text = "Cadastre-se";
+            // 
+            // Cadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 315);
+            this.Controls.Add(this.lblCadastreseTitulo);
+            this.Controls.Add(this.btnIncluirCadastro);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.txtConfirmarSenhaCadastro);
+            this.Controls.Add(this.txtSenhaCadastro);
+            this.Controls.Add(this.txtUsuarioCadastro);
+            this.Controls.Add(this.lblConfirmarSenhaCadastro);
+            this.Controls.Add(this.lblSenhaCadastro);
+            this.Controls.Add(this.lblUsuarioCadastro);
+            this.Name = "Cadastro";
+            this.Text = "Cadastro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUsuarioCadastro;
+        private System.Windows.Forms.Label lblSenhaCadastro;
+        private System.Windows.Forms.Label lblConfirmarSenhaCadastro;
+        private System.Windows.Forms.TextBox txtUsuarioCadastro;
+        private System.Windows.Forms.TextBox txtSenhaCadastro;
+        private System.Windows.Forms.TextBox txtConfirmarSenhaCadastro;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnIncluirCadastro;
+        private System.Windows.Forms.Label lblCadastreseTitulo;
+    }
+}
