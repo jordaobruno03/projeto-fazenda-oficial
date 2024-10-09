91021e752bb8ef0942bc73b718996465ce874725
jordaobruno03 <164689481+jordaobruno03@users.noreply.github.com>
2024-10-09 15:46:33 -0300

    Criei a CRUD Fornecedores

---
+++ .vs/Sistema  Fazenda/Sistema  Fazenda/Crud Fornecedor.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Sistema__Fazenda
+{
+    partial class Crud_Fornecedor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEditarFornecedor = new System.Windows.Forms.Button();
+            this.ExcluirFornecedor = new System.Windows.Forms.Button();
+            this.IncluirFornecedor = new System.Windows.Forms.Button();
+            this.btnVoltarFornecedor = new System.Windows.Forms.Button();
+            this.txtProdutoFornecedor = new System.Windows.Forms.TextBox();
+            this.txtCNPJFornecedor = new System.Windows.Forms.TextBox();
+            this.txtNomeFornecedor = new System.Windows.Forms.TextBox();
+            this.txtIDFornecedor = new System.Windows.Forms.TextBox();
+            this.lblProduto = new System.Windows.Forms.Label();
+            this.lblCNPJ = new System.Windows.Forms.Label();
+            this.lblNomeFornecedor = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.btnCarregarFornecedor = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.selecioneOProdutoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnEditarFornecedor
+            // 
+            this.btnEditarFornecedor.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnEditarFornecedor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditarFornecedor.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnEditarFornecedor.Location = new System.Drawing.Point(147, 260);
+            this.btnEditarFornecedor.Name = "btnEditarFornecedor";
+            this.btnEditarFornecedor.Size = new System.Drawing.Size(119, 23);
+            this.btnEditarFornecedor.TabIndex = 43;
+            this.btnEditarFornecedor.Text = "Editar";
+            this.btnEditarFornecedor.UseVisualStyleBackColor = false;
+            // 
+            // ExcluirFornecedor
+            // 
+            this.ExcluirFornecedor.BackColor = System.Drawing.Color.Red;
+            this.ExcluirFornecedor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExcluirFornecedor.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ExcluirFornecedor.Location = new System.Drawing.Point(281, 332);
+            this.ExcluirFornecedor.Name = "ExcluirFornecedor";
+            this.ExcluirFornecedor.Size = new System.Drawing.Size(119, 23);
+            this.ExcluirFornecedor.TabIndex = 42;
+            this.ExcluirFornecedor.Text = "Exluir";
+            this.ExcluirFornecedor.UseVisualStyleBackColor = false;
+            // 
+            // IncluirFornecedor
+            // 
+            this.IncluirFornecedor.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.IncluirFornecedor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.IncluirFornecedor.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.IncluirFornecedor.Location = new System.Drawing.Point(147, 332);
+            this.IncluirFornecedor.Name = "IncluirFornecedor";
+            this.IncluirFornecedor.Size = new System.Drawing.Size(119, 23);
+            this.IncluirFornecedor.TabIndex = 41;
+            this.IncluirFornecedor.Text = "Incluir";
+            this.IncluirFornecedor.UseVisualStyleBackColor = false;
+            // 
+            // btnVoltarFornecedor
+            // 
+            this.btnVoltarFornecedor.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnVoltarFornecedor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVoltarFornecedor.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnVoltarFornecedor.Location = new System.Drawing.Point(12, 332);
+            this.btnVoltarFornecedor.Name = "btnVoltarFornecedor";
+            this.btnVoltarFornecedor.Size = new System.Drawing.Size(119, 23);
+            this.btnVoltarFornecedor.TabIndex = 40;
+            this.btnVoltarFornecedor.Text = "Voltar";
+            this.btnVoltarFornecedor.UseVisualStyleBackColor = false;
+            // 
+            // txtProdutoFornecedor
+            // 
+            this.txtProdutoFornecedor.Location = new System.Drawing.Point(120, 224);
+            this.txtProdutoFornecedor.Name = "txtProdutoFornecedor";
+            this.txtProdutoFornecedor.Size = new System.Drawing.Size(200, 20);
+            this.txtProdutoFornecedor.TabIndex = 35;
+            // 
+            // txtCNPJFornecedor
+            // 
+            this.txtCNPJFornecedor.Location = new System.Drawing.Point(120, 174);
+            this.txtCNPJFornecedor.Name = "txtCNPJFornecedor";
+            this.txtCNPJFornecedor.Size = new System.Drawing.Size(200, 20);
+            this.txtCNPJFornecedor.TabIndex = 34;
+            // 
+            // txtNomeFornecedor
+            // 
+            this.txtNomeFornecedor.Location = new System.Drawing.Point(120, 124);
+            this.txtNomeFornecedor.Name = "txtNomeFornecedor";
+            this.txtNomeFornecedor.Size = new System.Drawing.Size(200, 20);
+            this.txtNomeFornecedor.TabIndex = 33;
+            // 
+            // txtIDFornecedor
+            // 
+            this.txtIDFornecedor.Location = new System.Drawing.Point(120, 74);
+            this.txtIDFornecedor.Name = "txtIDFornecedor";
+            this.txtIDFornecedor.Size = new System.Drawing.Size(200, 20);
+            this.txtIDFornecedor.TabIndex = 32;
+            // 
+            // lblProduto
+            // 
+            this.lblProduto.AutoSize = true;
+            this.lblProduto.Location = new System.Drawing.Point(70, 231);
+            this.lblProduto.Name = "lblProduto";
+            this.lblProduto.Size = new System.Drawing.Size(47, 13);
+            this.lblProduto.TabIndex = 27;
+            this.lblProduto.Text = "Produto:";
+            // 
+            // lblCNPJ
+            // 
+            this.lblCNPJ.AutoSize = true;
+            this.lblCNPJ.Location = new System.Drawing.Point(80, 181);
+            this.lblCNPJ.Name = "lblCNPJ";
+            this.lblCNPJ.Size = new System.Drawing.Size(37, 13);
+            this.lblCNPJ.TabIndex = 26;
+            this.lblCNPJ.Text = "CNPJ:";
+            // 
+            // lblNomeFornecedor
+            // 
+            this.lblNomeFornecedor.AutoSize = true;
+            this.lblNomeFornecedor.Location = new System.Drawing.Point(79, 131);
+            this.lblNomeFornecedor.Name = "lblNomeFornecedor";
+            this.lblNomeFornecedor.Size = new System.Drawing.Size(38, 13);
+            this.lblNomeFornecedor.TabIndex = 25;
+            this.lblNomeFornecedor.Text = "Nome:";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(93, 81);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(21, 13);
+            this.lblID.TabIndex = 24;
+            this.lblID.Text = "ID:";
+            // 
+            // btnCarregarFornecedor
+            // 
+            this.btnCarregarFornecedor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCarregarFornecedor.Location = new System.Drawing.Point(283, 2);
+            this.btnCarregarFornecedor.Name = "btnCarregarFornecedor";
+            this.btnCarregarFornecedor.Size = new System.Drawing.Size(119, 23);
+            this.btnCarregarFornecedor.TabIndex = 23;
+            this.btnCarregarFornecedor.Text = "Carregar";
+            this.btnCarregarFornecedor.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selecioneOProdutoToolStripMenuItem,
+            this.toolStripComboBox1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(410, 27);
+            this.menuStrip1.TabIndex = 22;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // selecioneOProdutoToolStripMenuItem
+            // 
+            this.selecioneOProdutoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1});
+            this.selecioneOProdutoToolStripMenuItem.Name = "selecioneOProdutoToolStripMenuItem";
+            this.selecioneOProdutoToolStripMenuItem.Size = new System.Drawing.Size(145, 23);
+            this.selecioneOProdutoToolStripMenuItem.Text = "Selecione o Fornecedor:";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // Crud_Fornecedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 384);
+            this.Controls.Add(this.btnEditarFornecedor);
+            this.Controls.Add(this.ExcluirFornecedor);
+            this.Controls.Add(this.IncluirFornecedor);
+            this.Controls.Add(this.btnVoltarFornecedor);
+            this.Controls.Add(this.txtProdutoFornecedor);
+            this.Controls.Add(this.txtCNPJFornecedor);
+            this.Controls.Add(this.txtNomeFornecedor);
+            this.Controls.Add(this.txtIDFornecedor);
+            this.Controls.Add(this.lblProduto);
+            this.Controls.Add(this.lblCNPJ);
+            this.Controls.Add(this.lblNomeFornecedor);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.btnCarregarFornecedor);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Crud_Fornecedor";
+            this.Text = "Crud_Fornecedor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEditarFornecedor;
+        private System.Windows.Forms.Button ExcluirFornecedor;
+        private System.Windows.Forms.Button IncluirFornecedor;
+        private System.Windows.Forms.Button btnVoltarFornecedor;
+        private System.Windows.Forms.TextBox txtProdutoFornecedor;
+        private System.Windows.Forms.TextBox txtCNPJFornecedor;
+        private System.Windows.Forms.TextBox txtNomeFornecedor;
+        private System.Windows.Forms.TextBox txtIDFornecedor;
+        private System.Windows.Forms.Label lblProduto;
+        private System.Windows.Forms.Label lblCNPJ;
+        private System.Windows.Forms.Label lblNomeFornecedor;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Button btnCarregarFornecedor;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem selecioneOProdutoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
---
+++ .vs/Sistema  Fazenda/Sistema  Fazenda/Crud Fornecedor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema__Fazenda
+{
+    public partial class Crud_Fornecedor : Form
+    {
+        public Crud_Fornecedor()
+        {
+            InitializeComponent();
+        }
+    }
+}
